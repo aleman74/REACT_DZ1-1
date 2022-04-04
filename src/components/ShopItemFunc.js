ef8a2d00ed71e483d3f84ccf2b37effa9882d73a
aleman74 <aleman@list.ru>
2022-04-04 20:56:52 +0300

    version

---
+++ src/components/ShopItemFunc.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
+
+import ProductInfo from '../models/ProductInfo';
+
+
+function ShopItemFunc(props)
+{
+    const {item} = props;
+
+    return (
+        <div className="main-content">
+            <h2>{item.brand}</h2>
+            <h1>{item.title}</h1>
+            <h3>{item.description}</h3>
+            <div className="description">{item.descriptionFull}</div>
+            <div className="highlight-window mobile">
+                <div className="highlight-overlay"></div>
+            </div>
+            <div className="divider"></div>
+            <div className="purchase-info">
+                <div className="price">{item.currency}{item.price}</div>
+                <button>Добавить в корзину</button>
+            </div>
+        </div>
+    );
+}
+
+ShopItemFunc.propTypes =
+    {
+        item:PropTypes.instanceOf(ProductInfo).isRequired
+    }
+
+export default ShopItemFunc;
